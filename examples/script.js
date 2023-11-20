ffb53d72031e23f3d73978f5271a960d1afe989d
codebien <2103732+codebien@users.noreply.github.com>
2023-11-20 14:19:19 +0100

    Support authentication client
    First implementation for the Kerbeos JS module.

--- examples/script.js
+++ examples/script.js
@@ -1,6 +1,7 @@
 import http from 'k6/http';
 import exec from 'k6/execution';
-import kerberos from 'k6/x/kerberos';
+import { fs } from 'k6/experimental/fs';
+import { krb5 } from 'k6/x/kerberos';
 
 export const options = {
   iterations: 1,
@@ -14,13 +15,51 @@ var users = [{
   password: "safe-loaded-secret-2"
 }];
 
-export default function() {
-  const user = users[exec.vu.idInTest];
-  const sessionToken = kerberos.authenticate(user.username, user.password);
+const	spn = "HTTP/http.example.com"
+const	realm = "EXAMPLE.COM"
 
-  let headers = {
-    Authorization: sessionToken.NegotiateHeader(), //`Negotiate ${kerberosToken}` } },
-  };
+const cfg;
+(async function () {
+	cfg = await open("krb5.ini"); // TODO: replace with a env var
+})();
 
+let session = null;
+
+export default function () {
+  if (session == null) {
+    const user = users[exec.vu.idInTest];
+    const krb5ini = readAll(cfg);
+    const kos = new Client(cfg);
+    const token = kos.authenticate(user.username, user.password, spn);
+    session = token.NegotiateHeader()
+  }
+
+  let headers = {Authorization: session};
   const res = http.get('https://test.k6.io', headers);
+}
+
+func readAll(file) {
+	const buffer = new Uint8Array(4);
+
+  let totalBytesRead = 0;
+	while (true) {
+		const bytesRead = await file.read(buffer);
+		if (bytesRead == null) {
+			// EOF
+			break;
+		}
+
+		totalBytesRead += bytesRead;
+
+		// If bytesRead is less than the buffer size, we've read the whole file
+		if (bytesRead < buffer.byteLength) {
+			break;
+		}
+	}
+
+	if totalByteRead === 0 {
+	  return null;
+	}
+
+	return buffer;
 }
