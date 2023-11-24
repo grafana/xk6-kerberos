621988ba2be26384e1a484312415788b4ddc3da5
Joan López de la Franca Beltran <joanjan14@gmail.com>
2023-11-24 14:42:58 +0100

    Fix end-to-end test

--- ci/k6/script.js
+++ ci/k6/script.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import {open} from "k6/experimental/fs";
+import exec from 'k6/execution';
 import {Client} from 'k6/x/kerberos';
 import {describe, expect} from 'https://jslib.k6.io/k6chaijs/4.3.4.3/index.js';
 
@@ -17,19 +17,22 @@ const pass = "testpwd"
 
 const spn = "HTTP/http.example.com"
 
-let cfg;
+let krb5cfg;
 (async function () {
-	cfg = await open("../config/krb5.conf"); // TODO: replace with a env var
+	krb5cfg = await open(${__ENV.KRB5_CONFIG}); // TODO: replace with a env var
 })();
 
 let session;
 
 export default async function () {
 	if (session == null) {
-		const krb5ini = await readAll(cfg);
-		const kos = new Client(user, realm, pass, krb5ini);
-		const token = kos.authenticate(spn);
-		session = token.negotiateHeader()
+		try {
+			const kos = new Client(user, realm, pass, krb5cfg);
+			const token = kos.authenticate(spn);
+			session = token.negotiateHeader()
+		} catch (e) {
+			exec.test.abort(e);
+		}
 	}
 
 	describe('Authenticated request', () => {
@@ -39,36 +42,4 @@ export default async function () {
 		expect(res.status, 'response status').to.equal(200);
 		expect(res.body, 'response body').to.include("It works!");
 	});
-}
-
-async function readAll(file) {
-	const buffer = new Uint8Array(1024);
-	let all = new Uint8Array(0);
-
-	let totalBytesRead = 0;
-	while (true) {
-		let bytesRead = await file.read(buffer);
-		if (bytesRead == null) {
-			// EOF
-			break;
-		}
-
-		// Append the read bytes to the total content
-		const tmp = new Uint8Array(all.length + bytesRead);
-		tmp.set(all, 0);
-		tmp.set(buffer.slice(0, bytesRead), all.length);
-		all = tmp;
-
-		// If bytesRead is less than the buffer size, we've read the whole file
-		totalBytesRead += bytesRead;
-		if (bytesRead < buffer.byteLength) {
-			break;
-		}
-	}
-
-	if (totalBytesRead === 0) {
-		return null;
-	}
-
-	return all;
 }
